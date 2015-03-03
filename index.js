de7c9872f686cd526fdbcabd3bf2c028f5fe445d
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-03 17:31:05 -0300

    Fix issue #15
    - Check if options were given, assign a empty object otherwise;
    - Path of the custom block script must be relative to this plug-in.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var through       = require('through2'),
     gutil         = require('gulp-util'),
+    path          = require('path'),
     HTMLProcessor = require('htmlprocessor'),
     PluginError   = gutil.PluginError;
 
 module.exports = function(options) {
 
+    if (typeof options === 'undefined') {
+        options = {};
+    }
     if (!options.customBlockTypes) {
         options.customBlockTypes = [];
     }
 
     // Add some custom block types.
-    options.customBlockTypes.push('./custom/replace.js');
+    options.customBlockTypes.push(path.join(__dirname, 'custom/replace.js'));
 
     var processor = new HTMLProcessor(options),
         content   = '';
